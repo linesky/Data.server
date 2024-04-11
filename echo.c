6670a69528880aacaea68ca1e7c92bc107a1df0e
sky line <32512284+linesky@users.noreply.github.com>
2024-04-11 17:44:46 +0100

    Add files via upload

---
+++ echo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(argc,argv)
+int argc;
+char *argv[];
+{
+	int nn;
+	int n;
+	if(argc>1){
+		for (n=0;n<argc;n++){
+			printf("%s ",argv[n]);
+		}
+		printf("\n");
+	}else{
+		return 1;
+	}
+	return 0;
+}
